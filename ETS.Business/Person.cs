9757946d09ccc5d609c591fba4e7a2d70eb19512
n-jeric <114877030+n-jeric@users.noreply.github.com>
2022-10-11 13:21:12 -0500

    Add project files.

---
+++ ETS.Business/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETS.Business
+{
+    abstract class Person
+    {
+        string firstName;
+        string lastName;
+
+        public Person(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+
+        public virtual string toString()
+        {
+            return $"{this.firstName},{this.lastName}";
+        }
+
+    }
+}
